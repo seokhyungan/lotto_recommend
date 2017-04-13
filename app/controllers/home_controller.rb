2d5ce856bba000dc701e7bd073eb21200ed59554
GanSeokHyun <seokhyun.gan@likelion.org>
2017-04-13 10:25:18 +0000

    쌍따옴표 제거

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,47 +3,46 @@ require "open-uri"
 
 class HomeController < ApplicationController
     
+    $lotto=(1..45).to_a.sample(7).sort
+
 
     def index
     #숫자 랜덤으로 출력 6자리
-    lotto=(1..45).to_a.sample(7).sort
-        @lotto = lotto
+    @lotto = $lotto
+        
     end
     
     
     def numberCheck
         
-    #숫자 랜덤으로 출력 6자리
-    lotto=(1..45).to_a.sample(7).sort   
-       
     #이번주 번호 파싱
     url = "http://nlotto.co.kr/gameResult.do?method=byWin"
     data = Nokogiri::HTML(open(url))
     
-    scrapNumbers = data.css('article div div p img').map{|i| i['alt']}
+    scrapNumbers = data.css('article div div p img').map{|i| i['alt']}.to_s.gsub('"', '')
     
     # 출력된 번호와 파싱된 번호 비교
-    
-    match_numbers = lotto & scrapNumbers
-    match_count = match_numbers.count
+    # match_numbers = $lotto & scrapNumbers
+    # match_count = match_numbers.count()
         
-        if match_count == 6
-                result ="1등"
-        elsif match_count == 5 && my_numbers.include?(bonus_number)
-                result = "2등"
-        elsif match_count == 4
-                result = "3등"
-        elsif match_count == 3
-                result = "4등"
-        elsif match_count == 2
-                result = "5등"
-        else    result = "꽝"
-        end
+    #     if match_count == 6
+    #             result ="1등"
+    #     elsif match_count == 5 && my_numbers.include?(bonus_number)
+    #             result = "2등"
+    #     elsif match_count == 4
+    #             result = "3등"
+    #     elsif match_count == 3
+    #             result = "4등"
+    #     elsif match_count == 2
+    #             result = "5등"
+    #     else    result = "꽝"
+    #     end
     
-    @lotto = lotto
+    @paramLotto = $lotto
+
     @scrapNumbers = scrapNumbers
-    @match_numbers=match_numbers
-    @result=result
+    # @match_numbers=match_numbers
+    # @result=result
     
     end
 end
