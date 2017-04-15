e4f8912fd4c3afb2f150c37a0db2273c7b03d0b4
GanSeokHyun <seokhyun.gan@likelion.org>
2017-04-15 07:20:47 +0000

    완료(전역변수라 새로고침해도 번호가 초기화가 안되는 문제 남음)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,45 +4,64 @@ require "open-uri"
 class HomeController < ApplicationController
     
     $lotto=(1..45).to_a.sample(7).sort
-
-
+    
+  
     def index
-    #숫자 랜덤으로 출력 6자리
-    @lotto = $lotto
-        
+        #숫자 랜덤으로 출력 6자리
+        @lotto = $lotto
+    
     end
     
     
     def numberCheck
         
-    #이번주 번호 파싱
-    url = "http://nlotto.co.kr/gameResult.do?method=byWin"
-    data = Nokogiri::HTML(open(url))
-    
-    scrapNumbers = data.css('article div div p img').map{|i| i['alt']}.to_s.gsub('"', '')
-    
-    # 출력된 번호와 파싱된 번호 비교
-    # match_numbers = $lotto & scrapNumbers
-    # match_count = match_numbers.count()
+        #이번주 번호 파싱
+        url = "http://nlotto.co.kr/gameResult.do?method=byWin"
+        data = Nokogiri::HTML(open(url))
         
-    #     if match_count == 6
-    #             result ="1등"
-    #     elsif match_count == 5 && my_numbers.include?(bonus_number)
-    #             result = "2등"
-    #     elsif match_count == 4
-    #             result = "3등"
-    #     elsif match_count == 3
-    #             result = "4등"
-    #     elsif match_count == 2
-    #             result = "5등"
-    #     else    result = "꽝"
-    #     end
+        #파싱한 번호 배열에 입력 및 "" 마크 제거  
+        scrapNumbers = data.css('article div div p img').map{|i| i['alt']}
+        scrapNumbers = scrapNumbers.map{|n|eval n}
     
-    @paramLotto = $lotto
-
-    @scrapNumbers = scrapNumbers
-    # @match_numbers=match_numbers
-    # @result=result
-    
+        #출력된 번호와 파싱된 번호 비교
+        match_numbers = $lotto & scrapNumbers 
+        match_count = match_numbers.count()
+           
+            #if match_count == 6
+            #        result ="1등"
+            #elsif match_count == 5 && my_numbers.include?(bonus_number)
+            #        result = "2등"
+            #elsif match_count == 4
+            #        result = "3등"
+            #elsif match_count == 3
+            #        result = "4등"
+            #elsif match_count == 2
+            #        result = "5등"
+            #else    result = "꽝"
+            #end
+            
+        
+            case match_count
+            when 6
+              result ="1등"
+            when 5
+              result ="2등"
+            when 4
+              result ="3등"
+            when 3
+              result ="4등"
+            when 2
+              result ="5등"
+            when 1
+              result ="6등"
+            else
+              result ="꽝"
+            end
+        
+        @paramLotto = $lotto
+        @scrapNumbers = scrapNumbers
+        @match_numbers=match_numbers
+        @result=result
+        
     end
 end
